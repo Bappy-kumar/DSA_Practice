de00dadd757d0ada7fdfc5e9da5f53a31c8a18f4
Bappy-kumar <bappykumar4221@gmail.com>
2025-12-14 22:15:13 +0545

    Updated files

---
+++ OOPS Concept Using C++/Function_Overloading.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+	return a+b;
+}
+
+int add(int a, int b, int c)
+{
+	return a+b+c;
+}
+
+int main()
+{
+	int a,b,c;
+	cout<<"Enter  numbers to add: "<<endl;
+	cin>>a>>b>>c;
+	
+	cout<<add(a,b)<<endl;
+	cout<<add(a,b,c)<<endl;
+	
+	
+	
+	
+	return 0;
+}
