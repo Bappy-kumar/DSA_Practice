4cc6dadd8e629840de2cd4203fdd1cd9e7d3f820
Bappy-kumar <bappykumar4221@gmail.com>
2025-12-14 22:25:45 +0545

    Updated files

---
+++ OOPS Concept Using C++/Operator_Overloading1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Number {
+	private:
+		int value;
+		
+	public:
+	
+	Number(int v)
+	{
+		value = v;
+	}
+	
+	void show() {
+		cout<< value << endl;
+	}	
+	
+	Number operator+(Number n) {
+		return Number(value + n.value);
+	}
+};
+
+int main()
+{
+	Number n1(5);
+	Number n2(10);
+	
+	Number n3 = n1 + n2;
+	n3.show();
+	return 0;
+}
